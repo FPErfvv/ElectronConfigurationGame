446e1043294eecc01f694b803268a9883a6963b8
FredProbst <github@fsd.probst.org>
2021-04-04 22:57:13 -0400

    updates .exe file

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ cd /cygdrive/c/Users/fprobst/Desktop/code/personal/ElectronConfigurationGame
 cp -af ElectronConfigurationGame.jar app/
 jpackage -i app -n ElectronGame \
         --main-class Main --main-jar ElectronConfigurationGame.jar \
-	--win-menu --vendor "Fred Probst" --app-version 0.1 --win-dir-chooser \
-	--win-menu-group ElectronGame --win-shortcut
+	--win-menu --vendor "Fred Probst" --app-version 0.2 --win-dir-chooser \
+	--win-menu-group ElectronGame --win-shortcut --verbose --win-per-user-install
